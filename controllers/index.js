ab181fdaa60d52a3f5515dbd5b33f3a78f598b21
Aaron Weaver <95551676+Atweaver7@users.noreply.github.com>
2022-04-12 14:54:52 -0700

    added handlebars basic layout

--- controllers/index.js
+++ controllers/index.js
@@ -5,17 +5,10 @@ const apiRoutes = require('./api');
 
 
 router.use('/', homeRoutes);
-
-
-router.use((req, res) => {
-    res.status(404).end();
-});
-
-
+// router.use((req, res) => {
+//     res.status(404).end();
+// });
 router.use('/api', apiRoutes);
-
-
 router.use('/dashboard', dashboardRoutes);
-
 
 module.exports = router;
